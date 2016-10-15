2f669f193572241a526d3951ebee2908073cdbb8
Zach Wolf <hello@zachwolf.com>
2016-10-15 18:08:48 -0500

    init commit

---
+++ src/components/CanvasComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//Canvas.css');
+
+class CanvasComponent extends React.Component {
+  render() {
+    return (
+      <div className="canvas-component">
+        Please edit src/components///CanvasComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+CanvasComponent.displayName = 'CanvasComponent';
+
+// Uncomment properties you need
+// CanvasComponent.propTypes = {};
+// CanvasComponent.defaultProps = {};
+
+export default CanvasComponent;
